1ef86fbfe4b464e6d980d44f1b86b6f556952523
FSauna <yfuta0630@gmail.com>
2021-09-14 12:24:07 +0000

    [Add] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,8 +12,11 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    if book.save
+      redirect_to book_path(book.id), flash: { notice: 'Book was successfully created.' }
+    else
+      redirect_to books_path
+    end
   end
 
   def edit
@@ -22,8 +25,11 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      redirect_to book_path(book.id), flash: { notice: 'Book was successfully updated.' }
+    else
+      redirect_to book_path(book)
+    end
   end
   
   def destroy
